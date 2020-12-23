93830bbe17721288ce0ec0d73c4c0e66afca847d
Oskar Sharipov <oskarsh[at]riseup[dot]io>
2020-12-23 10:47:53 +0300

    Generate new big test on 2800x2800 matrix

--- generate_test.cpp
+++ generate_test.cpp
@@ -14,24 +14,15 @@ int main(void) {
 	int n;
 	cin >> n >> LIMIT >> FLOAT_LIMIT;
 
-	cerr << n << "\n";
-	string for_sympy = "Matrix([";
+	cout << n << "\n";
 	for (int i = 0; i < n; ++i) {
-		for_sympy += "[";
 		for (int j = 0; j < n; ++j) {
 			int before_dot = arc4random() % LIMIT;
 			int after_dot = arc4random() % FLOAT_LIMIT;
 			double element = before_dot + (double)after_dot / FLOAT_LIMIT;
-			cerr << element << " ";
-			for_sympy += to_string(before_dot) + "." + to_string(after_dot);
-			for_sympy += (j + 1 == n ? "" : ",");
+			cout << element << " ";
 		}
-		cerr << "\n";
-		for_sympy += "]";
-		for_sympy += (i + 1 == n ? "" : ",");
+		cout << "\n";
 	}
-	for_sympy += "]).det()";
-
-	cout << for_sympy << "\n";
 }
 
